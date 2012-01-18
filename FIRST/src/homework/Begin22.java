028e38cdb69cc20b94dda9f95af9f7a0ba52fd9f
ihorkyptsiv <ihorkyptsiv@gmail.com>
2012-01-18 20:21:23 +0300

    2.Translation comment and changed code

--- FIRST/src/homework/Begin22.java
+++ FIRST/src/homework/Begin22.java
@@ -3,7 +3,7 @@ package homework;
 public class Begin22
 /*
 *Begin22. Test the validity of statements:"These numbers x, y are the coordinates point lying in the second coordinate
-*a quarter. "
+* a quarter. "
  */
 {
 	
@@ -13,7 +13,7 @@ public class Begin22
 		int x = 1;
 		int y = 2;
 		
-		if (x < 0 && y > 0)
+		if (x < 0 && y > 0 )
 		{
 			System.out.println(true);
 		} else
